5fbbb42c9f7d443adcbb5970c59da590f58a4612
Galib-23 <aagalib2323@gmail.com>
2024-01-08 14:07:02 +0600

    Winner Section Fixed

--- src/Pages/Home/Winner/Winner.jsx
+++ src/Pages/Home/Winner/Winner.jsx
@@ -1,13 +1,14 @@
 
-
+import winner from '../../../assets/winner.png';
 const Winner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZkOkJcz8xNsCQwSE62g5SMezVFOEJfI7Dozixgc87axWKHSfJHKHkHb85kA_R63JjhCI&usqp=CAU" className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={winner} className="w-[750px] h-[450px] rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Winner Of The Week!</h1>
-                    <p className="py-6">Steve Rogers</p>
+                    <h1 className="text-5xl font-bold text-cyan-400">Winner Of The Week!</h1>
+                    <p className="py-6 text-2xl font-semibold">Md. Abdul Latif</p>
+                    <p>Md. Abdul Latif recently participated in the annual business competetion 2023. He has successfully scored 500 a record score in the history of business competetion. He outpassed his competetors with a huge gap left behind.</p>
                 </div>
             </div>
         </div>
