343ffd5392a76cf69af09b554068917a7c1ff0ee
Galib-23 <aagalib2323@gmail.com>
2024-01-14 22:35:54 +0600

    color syncing fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--twc-background)',
+        text: 'var(--twc-text)',
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
